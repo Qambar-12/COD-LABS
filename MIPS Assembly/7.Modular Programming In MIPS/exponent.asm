ce23eacf2b592caa933af413e12c21203c906b04
Qambar-12 <144834362+Qambar-12@users.noreply.github.com>
2025-04-12 21:57:01 +0500

    Modular programming in MIPS

---
+++ MIPS Assembly/7.Modular Programming In MIPS/exponent.asm
@@ -0,0 +1,85 @@
+.data
+    msg1: .asciiz "THIS PROGRAM CALCULATES (NUMBER ^ POWER)\nENTER NUMBER (sum of digits of RollNo): "
+    msg2: .asciiz "ENTER POWER: "
+    msg3: .asciiz "RESULT: "
+    ERROR: .asciiz "ERROR: PLEASE ENTER POSITIVE POWER!\n"
+
+.text
+.globl main
+.ent main
+
+main:   
+
+#CALLER
+    li $v0, 4           # Print msg1
+    la $a0, msg1
+    syscall
+
+    li $v0, 5           # Read SUM of RollNO digits
+    syscall
+    move $a1, $v0       # SUM is moved in $a1
+
+    li $v0, 4           # Print msg2
+    la $a0, msg2
+    syscall
+
+    li $v0, 5           # Read POWER
+    syscall
+    move $a2, $v0       # POWER is moved in $a2
+
+    slti $t0, $a2, -1
+    beq $t0, $0, CALCULATE_POWER
+    
+    li $v0, 4           # Print ERROR
+    la $a0, ERROR
+    syscall
+    j main          
+
+    # We haven't used any temporary registers in main -> We do not have to push any register before function call
+    CALCULATE_POWER:
+    jal power_calculator
+
+    li $v0, 4           # Print msg3
+    la $a0, msg3
+    syscall
+
+    li $v0, 1          # Print Result
+    move $a0, $v1
+    syscall
+
+    li $v0, 10  # For explicit exit from main
+    syscall
+    jr $ra
+
+.end main
+
+
+.globl power_calculator
+.ent power_calculator
+#CALLER
+power_calculator:   
+    bne $a2, $0, CONTINUE
+    addi $v1, $0, 1
+    j END
+                        
+    CONTINUE:
+            addi $sp, $sp, -8   # Making room for 4 registers
+            sw $s0, 4($sp)      # Push $s0
+            sw $s1, 0($sp)      # Push $s1
+
+            addi $s0, $0, 1             # $s0 (Result) = 1 
+            addi $s1, $s1, 0            # $s1 (Iterator) = 0
+            LOOP:   mult $s0, $a1       # $s0 * $a1
+                    mflo $s0            # $s0 = $s0 * $a1
+                    addi $s1, $s1, 1    # $s1 += 1
+                    bne $s1, $a2, LOOP  # If $s1 != $a2 -> Jump To Loop
+
+            move $v1, $s0       # Move $s0 in $v1
+                        
+    END:
+    lw $s1, 0($sp)      # Pop $s1
+    lw $s0, 4($sp)      # Pop $s0
+    addi $sp, $sp, 8    # Adust SP        
+    jr $ra              # Return back
+                        
+.end power_calculator
